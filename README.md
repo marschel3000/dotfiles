1a1401d28e7fe694c87b03d8a4dd81ee10464a1d
Marcel Kade <kade.marcel@mailbox.org>
2017-08-25 14:34:00 +0200

    add script and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotfiles
+
+Execute "link.sh" to link these files to there default location.
+Don't forget to set your git user, email and signkey after that.
---
+++ link.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Absolute path this script is in
+SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
+
+HOSTNAME=$(uname -n)
+PLATFORM=$(uname -s)
+
+PARENT_DIRS=(
+	".gnupg"
+	".ssh"
+)
+
+LINK_TARGETS=(
+	".vimrc"
+	".gitconfig"
+	".gitignore"
+	".zshrc"
+	".screenrc"
+	".gnupg/gpg.conf"
+	".ssh/config"
+	".zsh"
+)
+
+# cleanup
+echo $'\nRemove:'
+	for ITEM in "${LINK_TARGETS[@]}"
+	do
+		rm -vrf "$HOME/$ITEM"
+	done
+
+echo $'\nMKDIR:'
+	for ITEM in "${PARENT_DIRS[@]}"
+	do
+		mkdir -pv "$HOME/$ITEM/"
+	done
+
+echo $'\nLink:'
+	for ITEM in "${LINK_TARGETS[@]}"
+	do
+		ln -v -s "$SCRIPTPATH/configs/$ITEM" "$HOME/$ITEM"
+	done
+
